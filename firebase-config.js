419056091a2dbbf32937848b2aa86008ba38a273
Mouhammed <156245214+CodeAndChronicles@users.noreply.github.com>
2026-06-15 12:18:11 +0300

    Update Firebase configuration and credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth }       from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore }  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getDatabase }   from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyAnLTYi24LQlXVehkbrUESbWrf0auGpz_I",
-  authDomain:        "nova-chat-24378.firebaseapp.com",
-  projectId:         "nova-chat-24378",
-  storageBucket:     "nova-chat-24378.firebasestorage.app",
-  messagingSenderId: "197888189650",
-  appId:             "1:197888189650:web:e3a4590c9d0236e49e390b",
-  databaseURL:       "https://nova-chat-24378-default-rtdb.firebaseio.com"
+    apiKey: "AIzaSyBP5RFGuNk1C76Zc4uKMqNlVbFhQlLMWIc",
+    authDomain: "novachat-6bc71.firebaseapp.com",
+    projectId: "novachat-6bc71",
+    storageBucket: "novachat-6bc71.firebasestorage.app",
+    messagingSenderId: "1054649466061",
+    appId: "1:1054649466061:web:2a141b642b9af00b971437",
+    measurementId: "G-FDH7W64K1C",
+    databaseURL: "https://novachat-6bc71-default-rtdb.firebaseio.com" // 🔥 أضف دي
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-export const db   = getFirestore(app);
-export const rtdb = getDatabase(app);
+export const db = getFirestore(app);
+export const rtdb = getDatabase(app); // 🔥 أضف دي
